64bbe65d9b130f68448df0d40eca6d3c280998ee
DorothyGay <dorothy.gay@ou.edu>
2017-12-11 00:17:15 +0000

    BAHLFAF

--- hello.js
+++ hello.js
@@ -1,3 +1,11 @@
 exports.world = function() {
   console.log('Hello World');
 }
+
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end('Hello Http');
+});
+server.listen(8080);
